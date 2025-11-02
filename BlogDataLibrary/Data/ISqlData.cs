10836d4224a71c0bdce364aad1aa636063d4443d
mporomero <mporomero@mymail.mapua.edu.ph>
2025-11-02 16:35:35 +0800

    Add project files.

---
+++ BlogDataLibrary/Data/ISqlData.cs
@@ -0,0 +1,12 @@
+﻿using BlogDataLibrary.Models;
+
+namespace BlogDataLibrary.Data
+{
+    public interface ISqlData
+    {
+        void AddPost(PostModel post);
+        UserModel Authenticate(string username, string password);
+        void Register(string username, string firstName, string lastName, string password);
+        ListPostModel ShowPostDetails(int id);
+    }
+}
---
+++ ROMEROBlogDB/Stored Procedures/spUsers_Authenticate.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[spUsers_Authenticate]
+    @username NVARCHAR(16),
+    @password NVARCHAR(16)
+AS
+begin
+    set nocount on;
+
+    SELECT [Id], [UserName], [FirstName], [LastName], [Password]
+    FROM dbo.Users
+    WHERE UserName = @username
+    AND Password = @password;
+end
